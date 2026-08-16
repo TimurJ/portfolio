f32f598f3ea4adbf3c24a3c71bbbbfd2ae94b171
Timur Jalilov <37907117+TimurJ@users.noreply.github.com>
2026-08-16 21:36:24 +0100

    Build Home section: base layout, header, and hero (#8)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://timurjalilov.com",
   vite: {
     plugins: [tailwindcss()],
   },
